e9526c16a1f016e7fb0a367681b33f7cb88118b7
a549232 <antonio.diaza@atos.net>
2016-02-24 16:18:03 +0100

    Update spring version. Add log4j.

--- 08 - HelloJPA/src/com/MyController.java
+++ 08 - HelloJPA/src/com/MyController.java
@@ -5,12 +5,17 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.apache.log4j.Logger;
+
 
 @Controller
 public class MyController {
 
-	@RequestMapping(value="/helloWeb", method=RequestMethod.GET)
+	 private static final Logger logger = Logger.getLogger(MyController.class);
+	
+	@RequestMapping(value="/helloWeb", method=RequestMethod.GET)	
 	public String wellcomePage(ModelMap modelMap) {
+		logger.debug("wellcomePage");
 		modelMap.addAttribute("message", "Spring 3 MVC Hello World");
 		return "hello";
 	}
