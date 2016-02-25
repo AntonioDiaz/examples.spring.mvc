84cbdebbb2e4d58655cdcee90d41eacd170ca0b6
a549232 <antonio.diaza@atos.net>
2016-02-25 09:35:29 +0100

    Person DAO implementation.

---
+++ 08 - HelloJPA/src/com/daos/PersonDAOImpl.java
@@ -0,0 +1,40 @@
+package com.daos;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.persistence.Person;
+
+@Repository
+@Transactional
+public class PersonDAOImpl implements PersonDAO {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
+	public void create(Person person) throws Exception {
+		entityManager.persist(person);
+	}
+
+	@Override
+	public void update(Person person) throws Exception {
+		entityManager.merge(person);
+	}
+
+	@Override
+	public void remove(Person person) throws Exception {
+		entityManager.remove(entityManager.contains(person) ? person : entityManager.merge(person));
+	}
+
+	@Override
+	public List<Person> getAllPerson() {
+		return entityManager.createQuery("Select a From UserEntity a", Person.class).getResultList();
+	}
+}
