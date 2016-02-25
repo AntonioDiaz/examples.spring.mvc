ca6865c40f9468b5caa2198db8cd16f20be06cc9
a549232 <antonio.diaza@atos.net>
2016-02-25 10:58:33 +0100

    Run code on init. Log system

---
+++ 01 - HelloController/src/com/InitDataBase.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.apache.log4j.Logger;
+
+public class InitDataBase {
+
+	private static final Logger logger = Logger.getLogger(InitDataBase.class);
+
+	public void init() {
+		logger.debug("init DATABASE");
+	}
+}
