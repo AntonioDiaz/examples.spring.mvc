332524e79ee644710d3625a699a2589e7ea593b8
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-03-18 21:07:08 +0100

    Showing how to use Value annotation.

--- 01 - HelloController/src/com/MyController.java
+++ 01 - HelloController/src/com/MyController.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MyController {
 
+	@Value("${mail_user}")
+	String mailUser;
+	@Value("${mail_password}")
+	String mailPassword;
 	
 	@RequestMapping(value="/helloWeb", method=RequestMethod.GET)
 	public String wellcomePage(ModelMap modelMap) {
+		modelMap.addAttribute("mail_user", mailUser);
+		modelMap.addAttribute("mail_password", mailPassword);
 		modelMap.addAttribute("message", "Spring 3 MVC Hello World");
 		return "hello";
 	}
